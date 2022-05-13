dd5d796191fa82f35e8abff28ba1b1afd907711a
Sungbin Jo <goranmoomin@daum.net>
2022-05-13 17:00:37 +0900

    Add kindly crazy test script

---
+++ test/kindly-crazy.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <sched.h>
+#include <unistd.h>
+
+#define SCHED_WRR 7
+int getcpu(unsigned int *cpu, unsigned int *node);
+
+int main(void)
+{
+	struct sched_param param = { .sched_priority = 0 };
+	int err = sched_setscheduler(getpid(), SCHED_WRR, &param);
+
+	if (err) {
+		perror("failed to set policy to SCHED_WRR");
+		return err;
+	}
+
+	for (;;) {
+		err = sched_yield();
+		if (err) {
+			perror("failed to yield");
+		}
+	}
+
+	return 0;
+}
